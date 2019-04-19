be99834edf9c3be185f386537dd1b3f37c79603e
Michael Schiumo <mschiumo18@gmail.com>
2019-04-19 13:57:57 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,22 +14,14 @@ class StudentsController < ApplicationController
 
   def edit
     @student = Student.find(params[:id])
-    redirect_to edit_student_path(@student)
   end
 
   def new
     @student = Student.new
-    redirect_to new_student_path(@student)
   end
 
   def show
     @student = Student.find(params[:id])
-    redirect_to student_path(@student)
   end
-
-  private do
-
-    def params
-      
 
 end
