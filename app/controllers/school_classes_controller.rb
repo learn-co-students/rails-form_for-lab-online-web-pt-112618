0b9123344b8825db74df1bbb3c502d55dc80aa8e
Michael Schiumo <mschiumo18@gmail.com>
2019-04-19 14:19:51 -0400

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -6,4 +6,22 @@ class SchoolClassesController < ApplicationController
     redirect_to school_class_path(@school_class)
   end
 
+  def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(params.require(:school_class).permit(:title, :room_number))
+    redirect_to student_path(@student)
+  end
+
+  def edit
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
 end
